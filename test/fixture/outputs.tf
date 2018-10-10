3fb9fc9b4d9d92fb055601f34bc2e32a592a45c4
Stuart Leeks <stuartle@microsoft.com>
2018-10-10 11:34:35 -0700

    Add admin login + password to outputs
    Also replicate in test fixture

--- test/fixture/outputs.tf
+++ test/fixture/outputs.tf
@@ -6,3 +6,10 @@ output "server_fqdn" {
   value = "${module.postgresql.server_fqdn}"
 }
 
+output "administrator_login" {
+  value = "${module.postgresql.administrator_login}"
+}
+
+output "administrator_password" {
+  value = "${module.postgresql.administrator_password}"
+}
