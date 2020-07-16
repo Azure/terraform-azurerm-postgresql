88cc1b3bc0b139014a7a357e4dcdac4507a221c5
yupwei68 <yupwei@microsoft.com>
2020-07-16 15:17:57 +0800

    update

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -15,30 +15,47 @@ resource "azurerm_resource_group" "test" {
   location = var.location
 }
 
+resource "azurerm_virtual_network" "test" {
+  name                = format("testVN%s", random_id.name.hex)
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = format("testSN%s", random_id.name.hex)
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.1.0/24"]
+  service_endpoints    = ["Microsoft.Sql"]
+}
+
 module "postgresql" {
   source = "../../"
 
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
 
-  server_name  = "postgresql${random_id.name.hex}"
-  sku_name     = "GP_Gen5_2"
+  server_name = "postgresql${random_id.name.hex}"
+  sku_name    = "GP_Gen5_2"
 
-  storage_mb            = 5120
-  backup_retention_days = 7
-  geo_redundant_backup_enabled  = false
-
-  administrator_login    = "azureuser"
-  administrator_password = "Azur3us3r!"
-
-  server_version  = "9.5"
-  ssl_enforcement_enabled = true
-
-  db_names     = var.db_names
-  db_charset   = "UTF8"
-  db_collation = "English_United States.1252"
+  storage_mb                   = 5120
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+  administrator_login          = "azureuser"
+  administrator_password       = "Azur3us3r!"
+  server_version               = "9.5"
+  ssl_enforcement_enabled      = true
+  db_names                     = var.db_names
+  db_charset                   = "UTF8"
+  db_collation                 = "English_United States.1252"
 
   firewall_rule_prefix = var.fw_rule_prefix
   firewall_rules       = var.fw_rules
+
+  vnet_rule_name_prefix = var.vnet_rule
+  vnet_rules = [
+    { name = "subnet1", subnet_id = azurerm_subnet.test.id }
+  ]
 }
 
--- test/fixture/terraform.tfvars
+++ test/fixture/terraform.tfvars
@@ -1,9 +1,11 @@
 location = "eastus"
 
-db_names = ["db1","db2"]
+db_names = ["db1", "db2"]
 
 fw_rule_prefix = "firewall"
 
 fw_rules = [
-  {name="test1", start_ip="10.0.0.5", end_ip="10.0.0.8"},
+  { name = "test1", start_ip = "10.0.0.5", end_ip = "10.0.0.8" },
 ]
+
+vnet_rule = "postgresql-vnet-rule-"
